5f2a7049a92dad6f73782503584850dd34412629
Jhan Frank <jhnfrankz@gmail.com>
2024-04-10 10:06:13 -0500

    Using store, detect which playlist is playing and fix ViewTransition

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,3 +1,4 @@
+import { usePlayerStore } from "@/store/playerStore";
 import { useEffect, useRef, useState } from "react";
 
 export const Pause = () => (
@@ -9,7 +10,7 @@ export const Play = () => (
 );
 
 export function Player() {
-  const [isPlaying, setIsPlaying] = useState(false);
+  const { isPlaying, setIsPlaying } = usePlayerStore(state => state);
   const [currentSong, setCurrentSong] = useState(null);
   const audioRef = useRef();
 
