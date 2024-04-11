54a690e46ffc91ad6899752e9dc633b9298bc022
Jhan Frank <jhnfrankz@gmail.com>
2024-04-10 19:48:06 -0500

    Added play and pause music, change music to other playlist  and show actual music in the Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { usePlayerStore } from "@/store/playerStore";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export const Pause = () => (
   <svg role="img" aria-hidden="true" viewBox="0 0 16 16" width="16" height="16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -9,30 +9,54 @@ export const Play = () => (
   <svg role="img" aria-hidden="true" viewBox="0 0 16 16" width="16" height="16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
 );
 
+const CurrentSong = ({ image, title, artists }) => {
+  return (
+    <div className={`
+      flex items-center gap-5 relative overflow-hidden
+    `}>
+      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+        <img src={image} alt={title} />
+      </picture>
+
+      <div className="flex flex-col">
+        <h3 className="font-semibold text-sm block">
+          {title}
+        </h3>
+        <span className="text-xs opacity-80">
+          {artists?.join(", ")}
+        </span>
+      </div>
+    </div>
+  );
+}
+
 export function Player() {
-  const { isPlaying, setIsPlaying } = usePlayerStore(state => state);
-  const [currentSong, setCurrentSong] = useState(null);
+  const { isPlaying, setIsPlaying, currentMusic } = usePlayerStore(state => state);
   const audioRef = useRef();
 
   useEffect(() => {
-    audioRef.current.src = `/music/1/01.mp3`;
-  }, []);
+    isPlaying
+      ? audioRef.current.play()
+      : audioRef.current.pause();
+  }, [isPlaying]); // when isPlaying changes, the effect is triggered
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.play();
+    }
+  }, [currentMusic]);
 
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-      audioRef.current.volume = 0.1;
-    }
-
     setIsPlaying(!isPlaying);
   }
 
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
       <div>
-        CurrentSong..
+        <CurrentSong {...currentMusic.song} />
       </div>
 
       <div className="grid place-content-center gap-4 flex-1">
